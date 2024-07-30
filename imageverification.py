f8b6551ceff807a0a2e615d0c1a2291d0ac6bd62
meranamkarthikeyavarma <seetarama07@gmail.com>
2024-07-30 18:56:05 +0530

    hi

--- imageverification.py
+++ imageverification.py
@@ -1,17 +1,18 @@
 import streamlit as st
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from pymongo.errors import ConnectionFailure
 
-uri = "mongodb+srv://seetarama07:LdrF4mPtz5zdQWsY@imageverification.nywflxg.mongodb.net/?retryWrites=true&w=majority&appName=imageverification"
+uri = "mongodb+srv://seetarama07:LdrF4mPtz5zdQWsY@imageverification.nywflxg.mongodb.net/?retryWrites=true&w=majority&ssl=false"
 
 st.write("Before creating client")
-client = MongoClient(uri, server_api=ServerApi('1'), serverSelectionTimeoutMS=100000)  # 5000 milliseconds = 5 seconds
+client = MongoClient(uri, server_api=ServerApi('1'), serverSelectionTimeoutMS=10000)
 st.write("After creating client")
 
 try:
     st.write("Before pinging")
     client.admin.command('ping')
     st.write("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
+except ConnectionFailure as e:
     st.write(e)
     st.write("Exception occurred")
