1173ac4e039dda2dacdca5585df84e25c682b557
meranamkarthikeyavarma <seetarama07@gmail.com>
2024-07-30 12:37:48 +0530

    hi

--- imageverification.py
+++ imageverification.py
@@ -10,7 +10,9 @@ client = MongoClient(mongo_uri)
 db = client[db_name]
 collection = db[collection_name]
 
-# # Example query
-# team_name = "example_team"
-# team = collection.find_one({"team_name": team_name})
-# print(team)
+
+
+# Example query
+team_name = "example_team"
+team = collection.find_one({"team_name": team_name})
+print(team)
