ff57060e301c1b7fd2f3d885722b5031db58fa1f
meranamkarthikeyavarma <seetarama07@gmail.com>
2024-07-30 18:41:55 +0530

    hi

--- imageverification.py
+++ imageverification.py
@@ -5,7 +5,7 @@ from pymongo.server_api import ServerApi
 uri = "mongodb+srv://seetarama07:LdrF4mPtz5zdQWsY@imageverification.nywflxg.mongodb.net/?retryWrites=true&w=majority&appName=imageverification"
 
 st.write("Before creating client")
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri, server_api=ServerApi('1'), serverSelectionTimeoutMS=10000)  # 5000 milliseconds = 5 seconds
 st.write("After creating client")
 
 try:
